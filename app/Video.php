cc5a03859b8e1b74ac4c25103304d875cbe6bf98
andela-doladosu <daradosu@gmail.com>
2015-10-24 21:42:38 +0000

    Add video model

---
+++ app/Video.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    protected $table = 'videos';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'url', 'category', 'description', 'user_id'];
+
+    protected function user()
+    {
+        $this->belongsTo('App\User');
+    }
+
+}
